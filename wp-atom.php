9c1eb766144336010bfb200d0ed565fcc9cee6b3
watersplash8 <pvelinov@hotmail.com>
2019-07-13 15:55:00 -0700

    Add files via upload

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="/feed/atom/">/feed/atom/</a></p>
+	</body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Log In &lsaquo; BM College &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org'/>
+<link rel='stylesheet' id='dashicons-css' href='/wp-includes/css/A.dashicons.min.css,qver=29442f740e367aa19a3f5f42c42ec16a.pagespeed.cf.LT4V6Htg4t.css' type='text/css' media='all'/>
+<link rel='stylesheet' id='buttons-css' href='/wp-includes/css/buttons.min.css?ver=29442f740e367aa19a3f5f42c42ec16a' type='text/css' media='all'/>
+<link rel='stylesheet' id='forms-css' href='/wp-admin/css/forms.min.css?ver=29442f740e367aa19a3f5f42c42ec16a' type='text/css' media='all'/>
+<link rel='stylesheet' id='l10n-css' href='/wp-admin/css/l10n.min.css?ver=29442f740e367aa19a3f5f42c42ec16a' type='text/css' media='all'/>
+<link rel='stylesheet' id='login-css' href='/wp-admin/css/login.min.css?ver=29442f740e367aa19a3f5f42c42ec16a' type='text/css' media='all'/>
+	<meta name='robots' content='noindex,noarchive'/>
+	<meta name='referrer' content='strict-origin-when-cross-origin'/>
+		<meta name="viewport" content="width=device-width"/>
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br/>
+		<input type="text" name="log" id="user_login" class="input" value="" size="20"/></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br/>
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20"/></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In"/>
+		<input type="hidden" name="redirect_to" value="/wp-admin/"/>
+		<input type="hidden" name="testcookie" value="1"/>
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/my-account/lost-password/">Lost your password?</a>
+</p>
+
+<script type="text/javascript">function wp_attempt_focus(){setTimeout(function(){try{d=document.getElementById('user_login');d.focus();d.select();}catch(e){}},200);}wp_attempt_focus();if(typeof wpOnload=='function')wpOnload();</script>
+
+	<p id="backtoblog"><a href="/">&larr; Back to BM College</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
